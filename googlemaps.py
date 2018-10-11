51378ff0a56dbdfdbebc81dabde7ae48c4fe87da
Rogoths <r.muny@hotmail.fr>
2018-10-11 22:15:53 +0200

    error messages in googlemaps.py

--- googlemaps.py
+++ googlemaps.py
@@ -18,17 +18,34 @@ class GoogleMaps:
         url = requests.get(self.url_base+parsed+self.key)
         #url = json.load(open("tour_eiffel.json"))
         data_raw = url.json()
+
         return data_raw
 
     def get_geocoding(self):
         data = self.request_data()
         data_results = data["results"]
+        data_error = data["status"]
 
-        for coords in data_results:
-            adress = coords["formatted_address"]
-            components = coords["geometry"]["location"]
-            long = components["lng"]
-            lat = components["lat"]
+        if data_error == "OK":
+            for coords in data_results:
+                adress = str("l'adresse de ta demande est ")+ coords["formatted_address"]
+                components = coords["geometry"]["location"]
+                long = components["lng"]
+                lat = components["lat"]
+
+                return long, lat, adress
+
+        elif data_error == "ZERO_RESULTS":
+            long = 0
+            lat = 0
+            adress = "Désolé. Je ne trouve aucun résultat :("
+
+            return long, lat, adress
+
+        else:
+            long = 0
+            lat = 0
+            adress = "Désolé. Il semble y avoir un problème d'allignement des planètes. Veuillez communiquer le message d'erreur suivant à mon créateur : "+str(data_error)
 
             return long, lat, adress
 
--- views.py
+++ views.py
@@ -13,7 +13,6 @@ def index():
 
     return render_template('base.html', key_googlemaps=KEY)
 
-
 @app.route('/ajax', methods=['GET', 'POST',])
 def user_query():
     response = {}
@@ -23,41 +22,14 @@ def user_query():
     normalize_text = parser.convert_ascii()
     response["text"] = normalize_text
     gmaps = GoogleMaps(normalize_text)
-    try:
-        long, lat, adress = gmaps.get_geocoding()
-        response["long"] = long
-        response["lat"] = lat
-        response["adress"] = "l'adresse de ta demande est "+adress
-        wiki = Mediawiki(lat, long)
-        m = random_messages()
-        response["extract"] = str(m)+wiki.get_info()
-
-        return jsonify(response)
-    except Exception as e:# -*- coding: utf-8 -*-
-        print("Erreur lors de la saisie de la requète:")
-        print(e)
-        response["extract"] = ""
-        response["adress"] = "Je suis navré... je n'ai pas compris ta demande. Tu peux réessayer si tu veux :)"
-        return jsonify(response)
-'''
-@app.route('/s', methods=['GET'])
-def ajax_request():
-    response = {}
-    text = request.args.get('query')
-    lower_text = text.lower()#lower the text
-    lower_text = answer.lower()
-    parser = Parser(lower_text)
-    normalize_text = parser.convert_ascii()
-    response["text"]=normalize_text
-    gmaps = GoogleMaps(normalize_text)
-    long, lat = gmaps.get_geocoding()
+    long, lat, adress = gmaps.get_geocoding()
     response["long"] = long
     response["lat"] = lat
+    response["adress"] = adress
     wiki = Mediawiki(lat, long)
-    extract = wiki.get_info()
-    #return render_template('base.html', variable=extract)
+    m = random_messages()
+    response["extract"] = str(m)+wiki.get_info()
     return jsonify(response)
-'''
 
 if __name__ == '__main__':
     app.run(debug=True)#developer mode no need to restart the server
